be2ec029726b2e5fdf0a4f06338f2f69c73b474c
Paul <paulsanchezsolisvega10@gmail.com>
2025-11-22 14:18:05 -0800

    Creación de la clase nodo

---
+++ src/logica/Nodo.java
@@ -0,0 +1,38 @@
+package logica;
+
+public class Nodo {
+
+    private Nodo siguiente;
+    private Nodo anterior;
+    private int dato;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        siguiente = null;
+        anterior = null;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public Nodo getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Nodo anterior) {
+        this.anterior = anterior;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+}
